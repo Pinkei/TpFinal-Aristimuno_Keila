ffbee22a9b0f9ec3e199795602b31b2b5b8cdf83
pink <pink@example.com>
2023-04-28 18:24:21 -0300

    agregue productos

---
+++ productos.js
@@ -0,0 +1,52 @@
+const productos =[
+ {
+    id: 1,
+          name: "Borde ballena 40x40",
+          precio: 1350,
+          role: "Borde para las pileta de fibra",
+          img:"/img/bordeballena40x40.png"
+},
+{
+    id: 2,
+          name: "Borde L",
+          precio: 1400,
+          role: "Bordes para las piletas de cemento",
+          img:"/img/bordeL.png"
+},
+{
+    id: 3,
+    name: "Esquinero borde L",
+    precio: 1550,
+    role: "Esquienros para pileta de cemento",
+    img:"/img/esquinerobordeL.png"
+  },
+  {
+    id: 4,
+    name: "Contracurva",
+    precio: 1350,
+    role: "Esquienros para pileta con arco romano",
+    img: "/img/contacurvo.png"
+  },
+  {
+    id: 5,
+    name: "Borde ballena 40x60",
+    precio: 1350,
+    role: "Para una vereda de 60cm",
+    img: "/img/bordeballena40x60.png"
+  },
+  {
+    id: 6,
+    name: "solarium",
+    precio: 1220,
+    role: "Para los arco romanos",
+    img: "/img/solarium40x40.png"
+  },
+  {
+    id: 7,
+    name: "BORDE BALLENA 40X20",
+    precio: 1020,
+    role: "Para una vereda de 20cm",
+    img: "./img/bordeballena40x20.png"
+  }
+  
+]
