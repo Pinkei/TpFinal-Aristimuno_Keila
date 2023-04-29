ddd7c856313cff332d26ab65de64093edfa4ae39
pink <pink@example.com>
2023-04-29 20:27:00 -0300

    pag inicio se ve

--- productos.js
+++ productos.js
@@ -1,42 +1,42 @@
 const productos =[
  {
     id: 1,
-          name: "Borde ballena 40x40",
+          name: "BORDE BALLENA 40x40",
           precio: 1350,
           role: "Borde para las pileta de fibra",
           img:"/img/bordeballena40x40.png"
 },
 {
     id: 2,
-          name: "Borde L",
+          name: "BORDE L",
           precio: 1400,
           role: "Bordes para las piletas de cemento",
           img:"/img/bordeL.png"
 },
 {
     id: 3,
-    name: "Esquinero borde L",
+    name: "ESQUINERO BORDE L",
     precio: 1550,
     role: "Esquienros para pileta de cemento",
     img:"/img/esquinerobordeL.png"
   },
   {
     id: 4,
-    name: "Contracurva",
+    name: "CONTRACURVA",
     precio: 1350,
     role: "Esquienros para pileta con arco romano",
     img: "/img/contacurvo.png"
   },
   {
     id: 5,
-    name: "Borde ballena 40x60",
+    name: "BORDE BALLENA 40x60",
     precio: 1350,
     role: "Para una vereda de 60cm",
     img: "/img/bordeballena40x60.png"
   },
   {
     id: 6,
-    name: "solarium",
+    name: "SOLARIUM",
     precio: 1220,
     role: "Para los arco romanos",
     img: "/img/solarium40x40.png"
@@ -50,3 +50,27 @@ const productos =[
   }
   
 ]
+const listaProductos = document.getElementById("lista-productos");
+
+productos.forEach(producto => {
+  const nuevoElemento = document.createElement("li");
+
+  const nombreProducto = document.createElement("h2");
+  nombreProducto.innerText = producto.name;
+  nuevoElemento.appendChild(nombreProducto);
+
+  const precioProducto = document.createElement("p");
+  precioProducto.innerText = `Precio: ${producto.precio}`;
+  nuevoElemento.appendChild(precioProducto);
+
+  const rolProducto = document.createElement("p");
+  rolProducto.innerText = `Rol: ${producto.role}`;
+  nuevoElemento.appendChild(rolProducto);
+
+  const imagenProducto = document.createElement("img");
+  imagenProducto.src = producto.img;
+  imagenProducto.alt = producto.name;
+  nuevoElemento.appendChild(imagenProducto);
+
+  listaProductos.appendChild(nuevoElemento);
+});
